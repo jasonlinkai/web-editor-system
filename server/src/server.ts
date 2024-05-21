4a5856dd473d83f50ef4b73ff2066e2277f4f418
Jacky <jacky@iyes.dev>
2024-05-22 00:22:34 +0800

    feat: upload image to s3

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,22 @@ import cors, { CorsOptions } from "cors";
 import fileUpload from "express-fileupload";
 import { Request as JWTRequest } from "express-jwt";
 import { v4 } from "uuid";
+import AWS from "aws-sdk";
 import ServerDatabase from "./database";
 import Auth from "./auth";
 import paths from "./paths";
 import { User } from "./database/models";
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: "us-east-1",
+  sslEnabled: false,
+  s3ForcePathStyle: true,
+});
+
+// Create an S3 object
+const s3 = new AWS.S3();
 
 export default class Server {
   private app: Application;
@@ -53,15 +65,29 @@ export default class Server {
           const ext = uploadedFile.name.split(".")[1];
           const newFileName = v4() + `.${ext}`;
 
-          const image = await this.serverDatabase.imageRepository.create({
-            url: newFileName,
-            userId: req.auth?.user.id,
-          });
-          return res.send({
-            code: 0,
-            message: "success",
-            data: image.url,
-          });
+          s3.upload(
+            {
+              Bucket: "jacky-web-editor",
+              Key: newFileName,
+              Body: uploadedFile.data,
+              Tagging: "public=yes",
+            },
+            async (err) => {
+              if (err) {
+                throw err;
+              } else {
+                const image = await this.serverDatabase.imageRepository.create({
+                  url: newFileName,
+                  userId: req.auth?.user.id,
+                });
+                return res.send({
+                  code: 0,
+                  message: "success",
+                  data: image.url,
+                });
+              }
+            }
+          );
         } catch (e) {
           return res.status(400).send({
             code: 400,
